75cea8ac3b9a8210f7afa240681eb77aed978ba1
radoslav <radoslav.h.todorov@gmail.com>
2017-03-24 07:27:32 +0100

    01 Done

---
+++ 01 Standalone/OWINStandalone/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace OWINStandalone
+{
+    //https://docs.microsoft.com/en-us/aspnet/web-api/overview/hosting-aspnet-web-api/use-owin-to-self-host-web-api
+    //Install-Package Microsoft.AspNet.WebApi.OwinSelfHost
+    class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
