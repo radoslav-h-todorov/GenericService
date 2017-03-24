b517d51e55cfcee13c733c67dc66cdadf3ce22df
radoslav <radoslav.h.todorov@gmail.com>
2017-03-24 07:28:41 +0100

    02 Done

---
+++ 02 Swagger/SwaggerApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+using Swashbuckle.Application;
+
+namespace SwaggerApp
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableSwagger(c => c.SingleApiVersion("v1", "A title for your API"))
+                .EnableSwaggerUi();
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
